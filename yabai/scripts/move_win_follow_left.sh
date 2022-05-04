4229ca32ba4838943356ae09a441a0453e0b8eb5
Slimenian <slimenian@gmail.com>
2022-05-04 13:48:24 +0900

    Added 4th space to keybinds

--- yabai/scripts/move_win_follow_left.sh
+++ yabai/scripts/move_win_follow_left.sh
@@ -1,5 +1,5 @@
 #!/bin/dash
 curWindowId="$(yabai -m query --windows --window | jq -re ".id")"
 
-$(yabai -m query --spaces --space | jq -re ".index" | xargs -I {} bash -c "if [[ '{}' = '1' ]]; then yabai -m window --space 3; elif [[ '{}' = '2' ]]; then yabai -m window --space 1;  elif [[ '{}' = '3' ]]; then yabai -m window --space 2; fi")
+$(yabai -m query --spaces --space | jq -re ".index" | xargs -I {} bash -c "if [[ '{}' = '1' ]]; then yabai -m window --space 4; elif [[ '{}' = '2' ]]; then yabai -m window --space 1; elif [[ '{}' = '3' ]]; then yabai -m window --space 2;  elif [[ '{}' = '4' ]]; then yabai -m window --space 3; fi")
 $(yabai -m window --focus "$curWindowId")
